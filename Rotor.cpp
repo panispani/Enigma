ac97661ad0eca3e138274deb07102f0af43f45a8
panpan2 <p.panayiotou2@gmail.com>
2016-10-17 10:15:23 +0100

    Terrible Rotor bug fixed

--- Rotor.cpp
+++ Rotor.cpp
@@ -6,16 +6,16 @@ Rotor::Rotor():times_rotated(0) {
 }
 
 char Rotor::rmap(char c) {
-    return wire_char[(c - 'A' - times_rotated + 26) % 26] + 'A';
+    return (wire_char[(c - 'A' + times_rotated + 26) % 26] - times_rotated + 26) % 26 + 'A';
 }
 
 char Rotor::map(char c) {
-    return char_wire[(c - 'A' + times_rotated) % 26] + 'A';
+    return (char_wire[(c - 'A' + times_rotated) % 26] - times_rotated +26) % 26 + 'A';
 }
 
 bool Rotor::turn_next() {
     times_rotated = (times_rotated + 1) % 26;
-    return times_rotated == 26;
+    return times_rotated == 0;
 }
 
 // magic numbers
